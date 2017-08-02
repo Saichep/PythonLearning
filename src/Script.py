0b5f5a3104ce1424c3a922db4ae6d81909862c0c
veersudhir83 <veersudhir83@gmail.com>
2017-08-02 12:46:48 +0530

    new functions

--- src/Script.py
+++ src/Script.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 
+from datetime import datetime
+
 __author__ = "Sudheer Veeravalli <veersudhir83@gmail.com>"
 __copyright__ = "Copyright (c) 2017. Sudheer Veeravalli. Free for general use and re-distribution"
 __license__ = "Public Domain"
 __version__ = "1.0"
 
+
 def sayHello():
     print('Welcome to Python!')
 
-def multiLineComment():
+
+def multi_line_comment():
     """
     multi
     line
     comment"""
     print('processed multi line comment')
 
-def learnVariables():
+
+def learn_variables():
     my_int = 7
     my_float = 1.23
     my_bool = True
@@ -24,13 +29,15 @@ def learnVariables():
     print('value of my_bool is =', my_bool)
     print('value of my_bool negated is =', not my_bool)
 
+
 # Indentation
-def countEggs():
+def count_eggs():
     eggs = 12
     return eggs
 
+
 # math operations
-def learnMathOperations():
+def learn_math_operations():
     x = 10
     y = 3
     float = 0.5
@@ -41,13 +48,15 @@ def learnMathOperations():
     print(x, "to power of (exponential)", y, "=", x ** y)
     print(x, "modulus", y, "=", x % y)
 
-def restaurantBillCalculator(mealCost, tax_percentage, tip_percentage, currency):
-    subTotal = mealCost + mealCost * (tax_percentage / 100)
+
+def restaurant_bill_calculator(meal_cost, tax_percentage, tip_percentage, currency):
+    subTotal = meal_cost + meal_cost * (tax_percentage / 100)
     total = subTotal + subTotal * (tip_percentage / 100)
     print("SubTotal with Tax = %.2f" % subTotal, currency)
     print("Total (including tax and tip) = %.2f" % total, currency)
 
-def stringIndex(stringVar, index):
+
+def string_index(string_var, index):
     """
     The string "PYTHON" has six characters, numbered 0 to 5, as shown below:
     +---+---+---+---+---+---+
@@ -56,17 +65,41 @@ def stringIndex(stringVar, index):
       0   1   2   3   4   5
     So if you wanted "Y", you could just type "PYTHON"[1] (always start counting from 0!)
     """
-    if index <= len(stringVar):
-        print(index, '\'th char of ',stringVar, 'is', stringVar[index-1])
+    if index <= len(string_var):
+        print("%d'th char of %s is %s" %(index, string_var,string_var[index - 1]))
     else:
-        print("Index out of range: Keep it <=", len(stringVar))
+        print("Index out of range: Keep it <= %s" %len(string_var))
 
-def finishedConcepts():
-    multiLineComment()
-    print('count of eggs is =',countEggs())
-    learnVariables()
-    learnMathOperations()
-    restaurantBillCalculator(44.50, 6.75, 15.0, "$")
+
+def play_with_strings(string_var):
+    string_index(string_var, 7)
+    print("%s in lower case= %s" %(string_var, string_var.lower()))
+    print("%s in upper case= %s" %(string_var, string_var.upper()))
+
+
+def ask_and_rephrase_about_me():
+    name = input("What is your name? ")
+    quest = input("What is your quest? ")
+    color = input("What is your favorite color? ")
+
+    print("Ah, so your name is %s, your quest is %s, " \
+          "and your favorite color is %s." % (name, quest, color))
+
+def print_date_and_time_pretty_format():
+    now = datetime.now()
+    print('Today is %s/%s/%s' % (now.month, now.day, now.year))
+    print('Time is %s:%s:%s' % (now.hour, now.minute, now.second))
+
+
+def finished_concepts():
+    multi_line_comment()
+    print('count of eggs is = %d' %(count_eggs()))
+    learn_variables()
+    learn_math_operations()
+    restaurant_bill_calculator(44.50, 6.75, 15.0, "$")
+    play_with_strings("Hello Python!")
+    ask_and_rephrase_about_me()
+    print_date_and_time_pretty_format()
 
 sayHello()
-stringIndex("Python", 7)
+#finished_concepts()
