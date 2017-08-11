8ef14c81bbee882c80b4053955f1f5199f8f6d54
veersudhir83 <veersudhir83@gmail.com>
2017-08-11 15:07:47 +0530

    added return types for MyMath methods

--- src/MyMath.py
+++ src/MyMath.py
@@ -6,11 +6,11 @@ __license__ = "Public Domain"
 __version__ = "1.0"
 
 
-def is_int(x: int):
+def is_int(x: int) -> bool:
     """
     Returns True if a number is a true integer; False otherwise
     :param x: int
-    :return: boolean
+    :return: bool
     """
     if x - round(x) == 0:  # If Difference between he number and it's rounded value is Zero, then it's a true integer
         return True
@@ -18,7 +18,7 @@ def is_int(x: int):
         return False
 
 
-def sum_of_digits(x: int):
+def sum_of_digits(x: int) -> int:
     """
     Returns the sum of all numbers in a given integer; if less than zero, it will return zero
     :param x: int
@@ -31,11 +31,11 @@ def sum_of_digits(x: int):
     return result
 
 
-def is_prime(x: int):
+def is_prime(x: int) -> bool:
     """
     Returns True if the given number is prime number; False otherwise
     :param x: int
-    :return: boolean
+    :return: bool
     """
     result = True
     if x <= 1:
@@ -47,7 +47,7 @@ def is_prime(x: int):
     return result
 
 
-def factorial(x: int):
+def factorial(x: int) -> int:
     """
     Returns factorial value for given number
     :param x: int
@@ -59,7 +59,7 @@ def factorial(x: int):
         return x * factorial(x-1)
 
 
-def biggest_number(*args):
+def biggest_number(*args) -> int:
     """
     Returns the biggest number in a given list of integers
     :param args: list
@@ -76,11 +76,11 @@ def biggest_number(*args):
         return None
 
 
-def smallest_number(*args):
+def smallest_number(*args) -> int:
     """
     Returns the smallest number in a given list of integers
     :param args: list
-    :return: Object
+    :return: int
     """
     valid = True
     for arg in args:
@@ -93,11 +93,11 @@ def smallest_number(*args):
         return None
 
 
-def distance_from_zero(arg):
+def distance_from_zero(arg) -> int:
     """
     Returns the distance from Zero
-    :param arg: Object
-    :return: int Object
+    :param arg: int/float
+    :return: int int
     """
     if type(arg) == int or type(arg) == float:
         return abs(arg)
@@ -105,11 +105,11 @@ def distance_from_zero(arg):
         return None
 
 
-def validate_collections(*args):
+def validate_collections(*args) -> bool:
     """
     Returns True if the parameters are all of the same type. Else returns False
     :param args: list
-    :return: boolean
+    :return: bool
     """
     valid = False
     for index in range(1, len(args)):
@@ -121,11 +121,11 @@ def validate_collections(*args):
     return valid
 
 
-def custom_add(*args):
+def custom_add(*args) -> object:
     """
     Adds/appends the two or more objects/collections into a single one
     :param args: Collection of objects
-    :return: Object
+    :return: object
     Usage:
         custom_add(["a"], ["b"]); custom_add([1, 2, 3], [4, 5, 6], [4, 5, 6])
         custom_add(1, 2);  custom_add(1.5, 2.3);  custom_add(1.5, 2.3, 3.8)
@@ -145,11 +145,11 @@ def custom_add(*args):
     return result
 
 
-def custom_subtract(*args):
+def custom_subtract(*args) -> object:
     """
     Subtracts/removes the two or more objects/collections into a single one
     :param args: Collection of objects
-    :return: Object
+    :return: object
     Usage:
         custom_subtract(["a"], ["b"]);  custom_subtract([1, 2, 3], [4, 5, 6], [4, 5, 6])
         custom_subtract(1, 2);  custom_subtract(1.5, 2.3);   custom_subtract(1.5, 2.3, 3.8)
