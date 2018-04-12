cbb53d1c194045560f31f5237830bf4211c66de9
Sudheer Veeravalli <veersudhir83@gmail.com>
2018-04-12 16:22:40 +0530

    Delete strings.py

--- src/strings.py
+++
@@ -1,6 +0,0 @@
-my_str = 'TestSlicingInPython'
-
-print(my_str[my_str.index("test".capitalize()):my_str.index("slicing".capitalize())])
-print(my_str[my_str.index("slicing".capitalize()):my_str.index("in".capitalize())])
-print(my_str[my_str.index("in".capitalize()):my_str.index("python".capitalize())])
-print(my_str[my_str.index("python".capitalize()):])
